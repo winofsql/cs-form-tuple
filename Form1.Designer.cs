adbb4b5970e1b87ee445a7507407582506465918
sworc <lightbox@nifty.com>
2022-08-17 11:31:18 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace form_02;
+
+partial class Form1
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.action = new System.Windows.Forms.Button();
+        //
+        // action
+        //
+        this.action.Location = new System.Drawing.Point(92, 62);
+        this.action.Name = "action";
+        this.action.Size = new System.Drawing.Size(75, 23);
+        this.action.TabIndex = 0;
+        this.action.Text = "実行";
+        this.action.UseVisualStyleBackColor = true;
+        this.action.Click += new System.EventHandler(this.action_Click);
+
+        this.components = new System.ComponentModel.Container();
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.Text = "Form1";
+        this.Controls.Add(this.action);
+        this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+
+    }
+
+    #endregion
+    private Button action;
+}
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+using System.Diagnostics;
+
+namespace form_02;
+
+public partial class Form1 : Form
+{
+    const string Language = "C#";
+    const string Platform = ".NET";
+    const string Version = "10.0";
+
+    // 定数の補間文字列( C# 10 )
+    const string FullProductName = $"{Platform} - Language: {Language} Version: {Version}";
+
+    const string D = "DBG";
+
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void action_Click(object sender, EventArgs e)
+    {
+        Debug.WriteLine($"{D}:{FullProductName}");
+
+        var tuple = TupleAction();
+
+        Debug.WriteLine($"{D}:{tuple.a} , {tuple.b}");
+
+        Person person = new Person();
+
+        if ( person.FirstName != null ) {
+            Debug.WriteLine($"{D}:NULL ではない");
+        }
+        if ( person.LastName == null ) {
+            Debug.WriteLine($"{D}:NULL である");
+        }
+
+    }
+
+    // タプルの戻り値を返すメソッド
+    private (double a, int b) TupleAction()
+    {
+        var t = (Sum: 4.5, Count: 3);
+        Debug.WriteLine($"{D}:Sum of {t.Count} elements is {t.Sum}.");
+
+        (double Sum, int Count) d = (4.5, 3);
+        Debug.WriteLine($"{D}:Sum of {d.Count} elements is {d.Sum}.");
+
+        var x = (4.5, 3);
+        Debug.WriteLine($"{D}:Sum of {x.Item2} elements is {x.Item1}.");
+
+        return( x.Item1, x.Item2 );
+    }
+
+    // 自動プロパティ初期化子
+    public class Person
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set; }
+
+    }
+}
